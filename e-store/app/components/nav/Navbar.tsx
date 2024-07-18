3752021c44ff3dc94609cfc000dd6cc2ace8a880
karnowsky1 <kestime9015@bths.edu>
2024-07-18 04:16:44 -0400

    Cart Provider is now able to calculate the total number of items in cart as well as the subtotal price for the purchase. The user is now able to see the total number of items in their cart within the navbar

--- e-store/app/components/nav/Navbar.tsx
+++ e-store/app/components/nav/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Container from '../Container'
 import Link from 'next/link'
 import { Redressed } from 'next/font/google'
+import CartCount from './CartCount'
 
 const redressed =Redressed({subsets: ['latin'], weight: ["400"]})
 
@@ -14,7 +15,7 @@ const Navbar = () => {
             <Link href="/" className={`${redressed.className} font-bold text-2xl`}>E-Store</Link>
             <div className="hidden md:block">Search</div>
             <div className="flex items-center gap-8 md:gap-12">
-              <div>Cart Count</div>
+              <CartCount />
               <div>User Menu</div>
             </div>
           </div>
