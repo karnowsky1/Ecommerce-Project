891132df88adc26e34ca81f0b9b90a831e3fc4d4
karnowsky1 <kestime9015@bths.edu>
2024-08-08 05:13:51 -0400

    implemented login and sign up functionality for the LoginForm component. Supplementary UI changes also added

--- e-store/app/components/nav/Navbar.tsx
+++ e-store/app/components/nav/Navbar.tsx
@@ -3,6 +3,7 @@ import Container from '../Container'
 import Link from 'next/link'
 import { Redressed } from 'next/font/google'
 import CartCount from './CartCount'
+import UserMenu from './UserMenu'
 
 const redressed =Redressed({subsets: ['latin'], weight: ["400"]})
 
@@ -16,7 +17,7 @@ const Navbar = () => {
             <div className="hidden md:block">Search</div>
             <div className="flex items-center gap-8 md:gap-12">
               <CartCount />
-              <div>User Menu</div>
+              <UserMenu />
             </div>
           </div>
         </Container>
