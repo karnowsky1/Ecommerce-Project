9529a0e01ee5bdec376fea989847e122a4162613
karnowsky1 <kestime9015@bths.edu>
2024-08-26 21:57:04 -0400

    able to get details for the current user via redux and apply UI elements for UserMenu based on their authorization level

--- e-store/app/components/nav/Navbar.tsx
+++ e-store/app/components/nav/Navbar.tsx
@@ -4,10 +4,14 @@ import Link from 'next/link'
 import { Redressed } from 'next/font/google'
 import CartCount from './CartCount'
 import UserMenu from './UserMenu'
+import { getCurrentUser } from '@/actions/getCurrentUser'
 
-const redressed =Redressed({subsets: ['latin'], weight: ["400"]})
+const redressed = Redressed({subsets: ['latin'], weight: ["400"]})
 
-const Navbar = () => {
+const Navbar = async () => {
+  const currentUser = await getCurrentUser()
+  
+
   return (
     <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
       <div className="py-4 border-b-[1px]"> 
@@ -17,7 +21,7 @@ const Navbar = () => {
             <div className="hidden md:block">Search</div>
             <div className="flex items-center gap-8 md:gap-12">
               <CartCount />
-              <UserMenu />
+              <UserMenu currentUser={currentUser}/>
             </div>
           </div>
         </Container>
