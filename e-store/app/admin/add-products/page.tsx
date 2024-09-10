2eb27a77f879ec2937cc99f9899c0517276da417
karnowsky1 <kestime9015@bths.edu>
2024-09-10 01:13:21 -0400

    implemented AddProductForm. The user is now able to add products and have the image uploaded to firebase and the product data uploaded to mongodb

--- e-store/app/admin/add-products/page.tsx
+++ e-store/app/admin/add-products/page.tsx
@@ -1,7 +1,24 @@
+import Container from "@/app/components/Container"
+import FormWrap from "@/app/components/FormWrap"
+import AddProductForm from "./AddProductForm"
+import { getCurrentUser } from "@/actions/getCurrentUser"
+import NullData from "@/app/components/NullData"
 
-const AddProducts = () => {
+const AddProducts = async() => {
+  const currentUser = await getCurrentUser()
+
+  if(!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="Oops! Access Denied!" />
+  }
+
   return (
-    <div>AddProducts</div>
+    <div className="p-8 ">
+      <Container>
+        <FormWrap>
+          <AddProductForm />
+        </FormWrap>
+      </Container>
+    </div>
   )
 }
 
