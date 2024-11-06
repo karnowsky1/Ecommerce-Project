40d8077f3c1f85fd4eced940db87aba5ce783949
karnowsky1 <kestime9015@bths.edu>
2024-11-05 20:11:11 -0500

    created manageOrders table

--- e-store/app/admin/manage-orders/page.tsx
+++ e-store/app/admin/manage-orders/page.tsx
@@ -1,7 +1,25 @@
+import React from 'react'
+import ManageOrdersClient from './ManageOrdersClient'
+import Container from '@/app/components/Container'
+import { getCurrentUser } from '@/actions/getCurrentUser'
+import NullData from '@/app/components/NullData'
+import getOrders from '@/actions/getOrders'
 
-const ManageOrders = () => {
+
+const ManageOrders = async() => {
+
+  const orders = await getOrders()
+  const currentUser = await getCurrentUser()
+
+  if(!currentUser || currentUser.role !== "ADMIN"){
+    return <NullData title="Oops! Access denied" />
+  }
   return (
-    <div>ManageOrders</div>
+    <div className="pt-8">
+      <Container>
+        <ManageOrdersClient orders = {orders}/>
+      </Container>
+    </div>
   )
 }
 
