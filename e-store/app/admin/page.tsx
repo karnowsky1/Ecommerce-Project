2aadd4ba03100bf17e792c8c3908a11b0c031e9f
karnowsky1 <kestime9015@bths.edu>
2024-11-09 18:50:08 -0500

    implemented summary section for admins dashboard

--- e-store/app/admin/page.tsx
+++ e-store/app/admin/page.tsx
@@ -1,7 +1,25 @@
+import getProducts from "@/actions/getProducts"
+import Summary from "./Summary"
+import getOrders from "@/actions/getOrders"
+import getUsers from "@/actions/getUsers"
+import Container from "../components/Container"
 
-const Admin = () => {
+const Admin = async () => {
+  const products = await getProducts({category: null}) 
+  const orders = await getOrders()
+  const users = await getUsers()
+
+
   return (
-    <div className="pt-8">Admin</div>
+    <div className="pt-8">
+      <Container>
+        <Summary 
+          products={products}
+          orders={orders}
+          users={users}
+          />
+      </Container>
+    </div>
   )
 }
 
